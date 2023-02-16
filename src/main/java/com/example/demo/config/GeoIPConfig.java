67163d289e37194111ef390472419ae5b9a69b21
chiheb <chiheb.fellah@gmail.com>
2023-02-16 12:09:15 +0100

    adding geolocation7

--- src/main/java/com/example/demo/config/GeoIPConfig.java
+++ src/main/java/com/example/demo/config/GeoIPConfig.java
@@ -4,20 +4,37 @@ import com.maxmind.geoip2.DatabaseReader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class GeoIPConfig {
 
     @Value("${spring.maxmind.database-path}") // This is the path to the GeoLite2 database
-   // @Value("src/main/resources/db/GeoLite2-City.mmdb")
+   // @Value("src/main/resources/db/GeoLite2City.mmdb")
     private String databasePath;
 
     @Bean
     public DatabaseReader databaseReader() throws IOException {
-        File database = new File(databasePath);
+       File database = new File(databasePath);
+
+
+       // ClassPathResourceLoader resourceLoader = new ClassPathResourceLoader();
+      //  Resource resource = resourceLoader.getResource("classpath:file.txt");
+       // InputStream inputStream = resource.getInputStream();
+
+     //   ClassPathResource resource = new ClassPathResource(databasePath);
+        //InputStream inputStream = resource.getInputStream();
+      //  ClassPathResource resource = new ClassPathResource(databasePath);
+       // InputStream database = resource.getInputStream();
+      // return new DatabaseReader.Builder(database).build();
         return new DatabaseReader.Builder(database).build();
     }
+
+
+
 }
